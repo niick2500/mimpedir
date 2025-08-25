2913019770d514938e123dc987c6318839b8a564
niick2500 <nr3900015@gmail.com>
2025-08-25 16:31:13 -0300

    Criando telas home e login com funcionamento básico sem banco de dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
     import 'package:flutter/material.dart';
-    import 'usuario.dart';
+    import 'tela_login.dart';
 
     void main() {
       runApp(const MyApp());
@@ -11,31 +11,9 @@
       @override
       Widget build(BuildContext context) {
 
-        Usuario u = Usuario(
-          codigo: 1,
-          login: "Nicolly123",
-          nome: "Nicolly",
-          senha: "@senhaforte123",
-        );
-
         return MaterialApp(
-            home: Scaffold(
-              appBar: AppBar(
-                  title: const Text("Meus Dados: "),
-              ),
-              body: Center(
-                child: Padding(padding: const EdgeInsets.all(18.0),
-                    child: Column(
-                      children: [
-                        Text("Nome: ${u.nome}"),
-                        Text("Código: ${u.codigo}"),
-                        Text("Login: ${u.login}"),
-                        Text("Senha: ${u.senha}")
-                    ],
-                  ),
-                ),
-              ),
-            ),
+          debugShowCheckedModeBanner: false,
+          home: TelaLogin(),
         );
       }
     }
--- lib/tela_login.dart
+++ lib/tela_login.dart
@@ -1,15 +1,57 @@
 import 'package:flutter/material.dart';
+import 'usuario.dart';
+import 'tela_home.dart';
 
 class TelaLogin extends StatelessWidget{
   TelaLogin({super.key});
+
+  final TextEditingController usuarioController = TextEditingController();
+  final TextEditingController senhaControler = TextEditingController();
+
+  Usuario u = Usuario(
+    codigo: 1,
+    senha: "@senhaforte123",
+    login: 'admin',
+    nome: 'Administrador'
+  );
 
       @override
       Widget build(BuildContext context) {
         return Scaffold(
           appBar: AppBar(title: const Text("Tela de Login")),
-          body: const Center(
-            child: Text("Tela de Login"),
-          ),
+          body: Padding(padding: const EdgeInsets.all(24.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  decoration: const InputDecoration(labelText: 'Usuário'),
+                  controller: usuarioController,
+                ),
+                const SizedBox(height: 40),
+                TextField(
+                  decoration: const InputDecoration(labelText: 'Senha'),
+                  obscureText: true,
+                  controller: senhaControler,
+                ),
+                const SizedBox(height: 40),
+                ElevatedButton(onPressed: (){
+
+                  if(u.login == usuarioController.text && u.senha == senhaControler.text){
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => TelaHome())
+                    );
+                  }else {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      SnackBar(content: Text("Login ou senha inválidos!!"))
+                    );
+                  }
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(content: Text("Login: ${usuarioController.text} senha: ${senhaControler.text}"))
+                  );
+                }, child: Text("Login"))
+                ],
+            ),
+          )
         );
       }
     }
